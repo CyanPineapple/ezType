d4e4d4d9c5babd96bb49eb88721bf657c3a0c56b
PoloGe <119010079@link.cuhk.edu.cn>
2022-12-06 17:27:28 +0800

    feat: boolean parse
    Signed-off-by: PoloGe <119010079@link.cuhk.edu.cn>

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,23 @@ use pest::Parser;
 struct IdentParser;
 
 fn main() {
-    let pairs = IdentParser::parse(Rule::ident_list, "a1 b2").unwrap_or_else(|e| panic!("{}", e));
+    let pairs = IdentParser::parse(Rule::boolexp, "true || false && true")
+        .unwrap_or_else(|e| panic!("{}", e));
 
     // Because ident_list is silent, the iterator will contain idents
     for pair in pairs {
         // A pair is a combination of the rule which matched and a span of input
-        println!("Rule:    {:?}", pair.as_rule());
-        println!("Span:    {:?}", pair.as_span());
-        println!("Text:    {}", pair.as_str());
+        println!("    Rule:    {:?}", pair.as_rule());
+        println!("    Span:    {:?}", pair.as_span());
+        println!("    Text:    {}", pair.as_str());
 
         // A pair can be converted to an iterator of the tokens which make it up:
         for inner_pair in pair.into_inner() {
             match inner_pair.as_rule() {
-                Rule::alpha => println!("Letter:  {}", inner_pair.as_str()),
-                Rule::digit => println!("Digit:   {}", inner_pair.as_str()),
+                Rule::_and => println!("And:  {}", inner_pair.as_str()),
+                Rule::_or => println!("Or:   {}", inner_pair.as_str()),
+                Rule::_true => println!("True:  {}", inner_pair.as_str()),
+                Rule::_false => println!("False:   {}", inner_pair.as_str()),
                 _ => unreachable!(),
             };
         }
